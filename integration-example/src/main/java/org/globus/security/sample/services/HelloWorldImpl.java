44e38cab3e3c79ef0c8c7beffad0ee7d1bd2cac7
Tom Howe <turtlebender@gmail.com>
2009-10-22 12:14:36 -0500

    updating poms for style check

--- integration-example/src/main/java/org/globus/security/sample/services/HelloWorldImpl.java
+++ integration-example/src/main/java/org/globus/security/sample/services/HelloWorldImpl.java
@@ -1,13 +1,14 @@
 package org.globus.security.sample.services;
 
 
-import com.ecerami.wsdl.helloservice_wsdl.HelloPortType;
+
+import com.ecerami.wsdl.helloservice.HelloPortType;
 
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
 @WebService(targetNamespace = "http://www.ecerami.com/wsdl/HelloService.wsdl", name = "Hello_PortType",
-portName = "Hello_Port", serviceName = "Hello_Service", endpointInterface = "com.ecerami.wsdl.helloservice_wsdl.HelloPortType")
+portName = "Hello_Port", serviceName = "Hello_Service", endpointInterface = "com.ecerami.wsdl.helloservice.HelloPortType")
 public class HelloWorldImpl implements HelloPortType {
 
     public String sayHello(@WebParam(partName = "firstName", name = "firstName", targetNamespace = "") String firstName) {
