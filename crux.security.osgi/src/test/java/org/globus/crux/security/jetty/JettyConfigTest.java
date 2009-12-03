4fc9f5629823323d1f56576a92f42fb9a3fcecc9
Tom Howe <turtlebender@gmail.com>
2009-12-03 10:28:42 -0600

    updating configuration code

---
+++ crux.security.osgi/src/test/java/org/globus/crux/security/jetty/JettyConfigTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 1999-2006 University of Chicago
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.globus.crux.security.jetty;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import org.globus.crux.security.internal.JettyConfigService;
+import org.globus.security.jetty.GlobusSslSocketConnector;
+
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.mortbay.jetty.Connector;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.nio.SelectChannelConnector;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+/**
+ * FILL ME
+ *
+ * @author ranantha@mcs.anl.gov
+ */
+@Test
+public class JettyConfigTest {
+    private Server spy;
+    List<Connector> connectors;
+
+    @BeforeTest
+    public void setup() throws Exception{
+        connectors = new ArrayList<Connector>();
+        spy = Mockito.spy(new Server());
+        Mockito.doNothing().when(spy).start();
+        Mockito.doNothing().when(spy).stop();
+        Mockito.doReturn(false).when(spy).isRunning();
+//        Mockito.doAnswer(new Answer(){
+//            public Object answer(InvocationOnMock invocation) throws Throwable {
+//                Object[] args = invocation.getArguments();
+//                for(Object o: args){
+//                    if(o instanceof Connector){
+//                        connectors.add((Connector) o);
+//                    }
+//                }
+//                return null;
+//            }
+//        }).when(spy).addConnector(Mockito.any(Connector.class));
+    }
+
+    public void testLoadProperties() throws Exception{
+        JettyConfigService service = new JettyConfigService(spy);
+        Properties properties = new Properties();
+        properties.load(getClass().getResourceAsStream("/test-jetty.cfg"));
+        service.updated(properties);
+        Mockito.verify(spy).isRunning();
+        Mockito.verify(spy, Mockito.times(2)).addConnector(Mockito.any(Connector.class));
+
+    }
+}
