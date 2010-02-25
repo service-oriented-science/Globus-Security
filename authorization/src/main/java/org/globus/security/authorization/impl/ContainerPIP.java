bffe31c1d872d59f904fc6c924585b91740493f4
Rachana Ananthakrishnan <ranantha@mcs.anl.gov>
2010-02-25 16:26:07 -0600

    Attempt at bootstrap pips
    Signed-off-by: Rachana Ananthakrishnan <ranantha@mcs.anl.gov>

---
+++ authorization/src/main/java/org/globus/security/authorization/impl/ContainerPIP.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 1999-2006 University of Chicago
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.globus.security.authorization.impl;
+
+import java.util.Calendar;
+
+import javax.inject.Inject;
+import javax.xml.namespace.QName;
+
+import org.globus.security.authorization.Attribute;
+import org.globus.security.authorization.AttributeException;
+import org.globus.security.authorization.AttributeIdentifier;
+import org.globus.security.authorization.BootstrapPIP;
+import org.globus.security.authorization.ChainConfig;
+import org.globus.security.authorization.CloseException;
+import org.globus.security.authorization.Constants;
+import org.globus.security.authorization.EntityAttributes;
+import org.globus.security.authorization.IdentityAttributeCollection;
+import org.globus.security.authorization.InitializeException;
+import org.globus.security.authorization.NonRequestEntities;
+import org.globus.security.authorization.RequestEntities;
+import org.globus.security.authorization.impl.GlobusContext;
+import org.globus.security.authorization.util.AttributeUtil;
+
+public class ContainerPIP implements BootstrapPIP {
+
+    @Inject
+    private GlobusContext context;
+
+    public void initialize(String chainName, String prefix, ChainConfig config)
+            throws InitializeException {
+
+    }
+
+    public void collectRequestAttributes(RequestEntities requestAttrs)
+            throws AttributeException {
+
+        String containerId = context.getContainerId();
+
+        // to get issuer
+        AttributeIdentifier containerIdentifier =
+                new AttributeIdentifier(Constants.CONTAINER_ATTRIBUTE_URI,
+                        Constants.STRING_DATATYPE_URI, true);
+        Attribute containerIdAttr =
+                new Attribute(containerIdentifier, null, Calendar.getInstance(), null);
+        IdentityAttributeCollection containerIdCollection =
+                new IdentityAttributeCollection();
+        containerIdAttr.addAttributeValue(containerId);
+        containerIdCollection.add(containerIdAttr);
+
+        EntityAttributes envEntity = requestAttrs.getEnvironment();
+        if (envEntity == null) {
+            envEntity = new EntityAttributes(containerIdCollection);
+            requestAttrs.setEnvironment(envEntity);
+        } else {
+            envEntity.addIdentityAttributes(containerIdCollection);
+        }
+
+        /* Need service endpoint here
+        // service
+        String servicePath = ContextUtils.getTargetServicePath(this.context);
+        // base URL
+        URL baseURL = null;
+        try {
+            baseURL = ServiceHost.getBaseURL(this.context);
+        } catch (IOException ioe) {
+            throw new AttributeException(ioe);
+        }
+
+        AttributeIdentifier serviceIden = AttributeUtil.getServiceIdentifier();
+        // since this is done per operaiton, the validity can be infinity
+        Attribute serviceAttribute =
+                new Attribute(serviceIden, containerIssuer, now, null);
+        serviceAttribute.addAttributeValue(baseURL + servicePath);
+        IdentityAttributeCollection attrCol =
+                new IdentityAttributeCollection();
+        attrCol.add(serviceAttribute);
+
+        EntityAttributes resourceEntity = requestAttrs.getResource();
+        if (resourceEntity == null) {
+            resourceEntity = new EntityAttributes(attrCol);
+            requestAttrs.setResource(resourceEntity);
+        } else {
+            resourceEntity.addIdentityAttributes(attrCol);
+        }
+
+*/
+
+        QName operation = context.getOperation();
+        Attribute operationAttribute =
+                new Attribute(AttributeUtil.getOperationAttrIdentifier(),
+                        envEntity, Calendar.getInstance(), null);
+        operationAttribute.addAttributeValue(operation);
+        IdentityAttributeCollection attrCol = new IdentityAttributeCollection();
+        attrCol.add(operationAttribute);
+
+        EntityAttributes actionEntity = requestAttrs.getAction();
+        if (actionEntity == null) {
+            actionEntity = new EntityAttributes(attrCol);
+            requestAttrs.setAction(actionEntity);
+        } else {
+            actionEntity.addIdentityAttributes(attrCol);
+        }
+    }
+
+    public NonRequestEntities
+    collectAttributes(RequestEntities requestAttr) throws AttributeException {
+        return null;
+    }
+
+    public void close() throws CloseException {
+    }
+}
---
+++ authorization/src/main/java/org/globus/security/authorization/impl/X509BootstrapPIP.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 1999-2006 University of Chicago
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.globus.security.authorization.impl;
+
+import java.util.Calendar;
+import java.util.Set;
+
+import javax.inject.Inject;
+import javax.security.auth.Subject;
+
+import org.globus.security.authorization.Attribute;
+import org.globus.security.authorization.AttributeException;
+import org.globus.security.authorization.AttributeIdentifier;
+import org.globus.security.authorization.BootstrapPIP;
+import org.globus.security.authorization.ChainConfig;
+import org.globus.security.authorization.CloseException;
+import org.globus.security.authorization.EntityAttributes;
+import org.globus.security.authorization.IdentityAttributeCollection;
+import org.globus.security.authorization.InitializeException;
+import org.globus.security.authorization.NonRequestEntities;
+import org.globus.security.authorization.RequestEntities;
+import org.globus.security.authorization.util.AttributeUtil;
+
+public class X509BootstrapPIP implements BootstrapPIP {
+
+    @Inject
+    private GlobusContext context;
+
+    public void initialize(String chainName, String prefix_,
+                           ChainConfig config) throws InitializeException {
+
+    }
+
+    public void collectRequestAttributes(RequestEntities requestAttrs)
+            throws AttributeException {
+
+        Subject peerSubject = context.getPeerSubject();
+        if (peerSubject != null) {
+            IdentityAttributeCollection idenAttrColl =
+                    new IdentityAttributeCollection();
+            Attribute subjectAttribute =
+                    new Attribute(AttributeUtil.getPeerSubjectAttrIdentifier(),
+                            requestAttrs.getEnvironment(), Calendar.getInstance(), null);
+            subjectAttribute.addAttributeValue(peerSubject);
+            idenAttrColl.add(subjectAttribute);
+
+            Set peerPrincipals = peerSubject.getPrincipals();
+            if (!peerPrincipals.isEmpty()) {
+                AttributeIdentifier identifier =
+                        AttributeUtil.getPrincipalIdentifier();
+                Attribute principalAttribute =
+                        new Attribute(identifier, requestAttrs.getEnvironment(), Calendar.getInstance(), null,
+                                peerPrincipals);
+                idenAttrColl.add(principalAttribute);
+            }
+
+            EntityAttributes coll = requestAttrs.getRequestor();
+            if (coll == null) {
+                coll = new EntityAttributes(idenAttrColl);
+                requestAttrs.setRequestor(coll);
+            } else {
+                coll.addIdentityAttributes(idenAttrColl);
+            }
+        }
+    }
+
+    public NonRequestEntities collectAttributes(RequestEntities requestAttr) {
+        return null;
+    }
+
+    public void close() throws CloseException {
+    }
+}
