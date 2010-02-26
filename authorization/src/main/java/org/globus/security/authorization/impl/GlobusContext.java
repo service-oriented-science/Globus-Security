0db0f1319618d6594c407fce524a58b3e0e3da7c
Rachana Ananthakrishnan <ranantha@mcs.anl.gov>
2010-02-26 10:06:08 -0600

    contianer entity in context
    Signed-off-by: Rachana Ananthakrishnan <ranantha@mcs.anl.gov>

--- authorization/src/main/java/org/globus/security/authorization/impl/GlobusContext.java
+++ authorization/src/main/java/org/globus/security/authorization/impl/GlobusContext.java
@@ -3,21 +3,24 @@ package org.globus.security.authorization.impl;
 import javax.security.auth.Subject;
 import javax.xml.namespace.QName;
 
+import org.globus.security.authorization.EntityAttributes;
 
 public interface GlobusContext {
-	
+
 	<T> T get(Class<T> type);
-	
+
 	Object get(String key);
-	
+
 	String getContainerId();
-	
+
 	Subject getContainerSubject();
-	
+
 	Subject getPeerSubject();
-	
+
 	Subject getServiceSubject();
-	
+
 	QName getOperation();
+
+	EntityAttributes getContainerEntity();
 
 }
