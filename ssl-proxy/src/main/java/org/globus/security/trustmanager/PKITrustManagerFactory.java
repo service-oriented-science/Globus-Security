1ed218772f4359161d99514de2d529f6b0ea1619
Tom Howe <turtlebender@localhost.(none)>
2010-03-30 19:43:12 -0500

    factored out spring dependency

--- ssl-proxy/src/main/java/org/globus/security/trustmanager/PKITrustManagerFactory.java
+++ ssl-proxy/src/main/java/org/globus/security/trustmanager/PKITrustManagerFactory.java
@@ -27,65 +27,79 @@ import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactorySpi;
 
 import org.globus.security.X509ProxyCertPathParameters;
+import org.globus.security.provider.GlobusTrustManagerFactoryParameters;
 
 /**
  * This factory creates trust managers which support the Globus SSL library.
- *
+ * 
  * @version ${version}
  * @since 1.0
  */
 public class PKITrustManagerFactory extends TrustManagerFactorySpi {
 
-    private Collection<TrustManager> trustManagers = new ArrayList<TrustManager>();
-    
-    /**
-     * Initializes this factory with a source of certificate authorities and related trust material.
-     *
-     * @param keyStore The key store or null
-     * @throws KeyStoreException if the initialization fails.
-     */
-    @Override
-    protected void engineInit(KeyStore keyStore) throws KeyStoreException {
-        try {
-            this.engineInit(new CertPathTrustManagerParameters(new X509ProxyCertPathParameters(keyStore, null, null,
-                    false)));
-        } catch (InvalidAlgorithmParameterException e) {
-            throw new KeyStoreException(e);
-        }
-    }
+	private Collection<TrustManager> trustManagers = new ArrayList<TrustManager>();
 
-    /**
-     * Initializes this factory with a source of provider-specific key material.
-     * In some cases, initialization parameters other than a keystore may be needed by a provider.
-     * Users of that particular provider are expected to pass an implementation of the appropriate
-     * ManagerFactoryParameters as defined by the provider. The provider can then call the specified
-     * methods in the ManagerFactoryParameters implementation to obtain the needed information.
-     * <p/>
-     * This implementation requires X509ProxyCertPathParameters.
-     *
-     * @param managerFactoryParameters The X509ProxyCertPathParameters which are used to create TrustManagers.
-     * @throws InvalidAlgorithmParameterException
-     *          If the Parameters are invalid
-     */
-    @Override
-    protected void engineInit(ManagerFactoryParameters managerFactoryParameters)
-            throws InvalidAlgorithmParameterException {
-        if (managerFactoryParameters instanceof X509ProxyCertPathParameters) {
-            X509ProxyCertPathParameters ptmfp = (X509ProxyCertPathParameters) managerFactoryParameters;
-            trustManagers.add(new PKITrustManager(new X509ProxyCertPathValidator(), ptmfp));
-        } else {
-            throw new InvalidAlgorithmParameterException("Factory cannot accept parameters of type: " +
-                    managerFactoryParameters.getClass().getCanonicalName());
-        }
-    }
+	/**
+	 * Initializes this factory with a source of certificate authorities and
+	 * related trust material.
+	 * 
+	 * @param keyStore
+	 *            The key store or null
+	 * @throws KeyStoreException
+	 *             if the initialization fails.
+	 */
+	@Override
+	protected void engineInit(KeyStore keyStore) throws KeyStoreException {
+		try {
+			this
+					.engineInit(new CertPathTrustManagerParameters(
+							new X509ProxyCertPathParameters(keyStore, null,
+									null, false)));
+		} catch (InvalidAlgorithmParameterException e) {
+			throw new KeyStoreException(e);
+		}
+	}
 
-    /**
-     * Returns one trust manager for each type of trust material.
-     *
-     * @return The collection of TrustManagers
-     */
-    @Override
-    protected TrustManager[] engineGetTrustManagers() {
-        return trustManagers.toArray(new TrustManager[trustManagers.size()]);
-    }
+	/**
+	 * Initializes this factory with a source of provider-specific key material.
+	 * In some cases, initialization parameters other than a keystore may be
+	 * needed by a provider. Users of that particular provider are expected to
+	 * pass an implementation of the appropriate ManagerFactoryParameters as
+	 * defined by the provider. The provider can then call the specified methods
+	 * in the ManagerFactoryParameters implementation to obtain the needed
+	 * information.
+	 * <p/>
+	 * This implementation requires X509ProxyCertPathParameters.
+	 * 
+	 * @param managerFactoryParameters
+	 *            The X509ProxyCertPathParameters which are used to create
+	 *            TrustManagers.
+	 * @throws InvalidAlgorithmParameterException
+	 *             If the Parameters are invalid
+	 */
+	@Override
+	protected void engineInit(ManagerFactoryParameters managerFactoryParameters)
+			throws InvalidAlgorithmParameterException {
+		if (managerFactoryParameters instanceof GlobusTrustManagerFactoryParameters) {
+			GlobusTrustManagerFactoryParameters ptmfp = (GlobusTrustManagerFactoryParameters) managerFactoryParameters;
+			trustManagers.add(new PKITrustManager(
+					new X509ProxyCertPathValidator(), ptmfp
+							.getCertPathParameters()));
+		} else {
+			throw new InvalidAlgorithmParameterException(
+					"Factory cannot accept parameters of type: "
+							+ managerFactoryParameters.getClass()
+									.getCanonicalName());
+		}
+	}
+
+	/**
+	 * Returns one trust manager for each type of trust material.
+	 * 
+	 * @return The collection of TrustManagers
+	 */
+	@Override
+	protected TrustManager[] engineGetTrustManagers() {
+		return trustManagers.toArray(new TrustManager[trustManagers.size()]);
+	}
 }
