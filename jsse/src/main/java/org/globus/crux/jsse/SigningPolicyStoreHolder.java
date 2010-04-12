0fcca80a368a0578f57cfc0c4bf3200747e7915f
Tom Howe <turtlebender@localhost.(none)>
2010-04-12 18:12:00 -0500

    moving core modules to separate project

--- jsse/src/main/java/org/globus/crux/jsse/SigningPolicyStoreHolder.java
+++
@@ -1,7 +0,0 @@
-package org.globus.crux.jsse;
-
-import org.globus.security.provider.SigningPolicyStore;
-
-public interface SigningPolicyStoreHolder extends NamedSecurityObject {
-	public SigningPolicyStore getSigningPolicyStore();
-}
--- jsse/src/test/java/org/globus/security/jaas/JAASTest.java
+++
@@ -1,5 +0,0 @@
-package org.globus.security.jaas;
-
-public interface JAASTest {
-
-}
