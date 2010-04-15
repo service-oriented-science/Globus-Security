cb64368a9705a0ecfe251ac2d481e9be161cbef4
Tom Howe <turtlebender@gmail.com>
2010-04-15 09:28:32 -0500

    moved experimental modules to osgi branch

--- org.globus.crux.osgi-proxy/src/main/java/org/globus/crux/osgi/proxy/Constants.java
+++
@@ -1,7 +0,0 @@
-package org.globus.crux.osgi.proxy;
-
-public final class Constants {
-	public static final String PROXY_REQUIRED = "proxyRequired";
-	public static final String PROXY_SERVICE = "proxyService";
-	public static final String DO_NOT_PROXY = "doNotProxy";
-}
